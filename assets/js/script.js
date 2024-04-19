eb980845e59866865c276fb6dcfc6e3beb5a6380
GuilhermeGoncalvesTorres <guilherme.torres20@hotmail.com>
2024-04-19 18:09:55 -0300

    Final

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,9 +7,9 @@ const relogio = setInterval(function time() {
     let minutinhos = dateToday.getMinutes();
     let segundinhos = dateToday.getSeconds();
 
-    if(horinhas < 10) horinhas = 0 + horinhas;
-    if(minutinhos < 10) minutinhos = 0 + minutinhos;
-    if(segundinhos < 10) segundinhos = 0 + segundinhos;
+    if(horinhas < 10) horinhas = '0' + horinhas;
+    if(minutinhos < 10) minutinhos = '0' + minutinhos;
+    if(segundinhos < 10) segundinhos = '0' + segundinhos;
 
     horas.textContent = horinhas;
     minutos.textContent = minutinhos;
